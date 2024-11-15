5a5f725c938ce5a13fc17560520be583a01b2e2b
Maxence <maxence.ogier@gmail.com>
2024-11-15 11:39:21 +0100

    Tp-3

--- src/test/java/java17/ex06/Function_06_Test.java
+++ src/test/java/java17/ex06/Function_06_Test.java
@@ -18,7 +18,8 @@ public class Function_06_Test {
     // TODO la méthode retourne une chaîne de caractères de la forme [age=<AGE>] (exemple : [age=12])
     String formatAge(Supplier<Person> supplier) {
         // TODO
-        return null;
+        Person p = supplier.get();
+        return "[age=" + p.getAge() +"]";
     }
     // end::formatAge[]
 
@@ -26,7 +27,7 @@ public class Function_06_Test {
     @Test
     public void test_supplier_formatAge() throws Exception {
         // TODO compléter le test unitaire pour qu'il soit passant
-        String result = formatAge(null);
+        String result = formatAge(() -> new Person("toto", "tata", 35, "secret"));
 
         assert result.equals("[age=35]");
     }
